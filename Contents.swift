53729ed8ad3e1424c3c7ae242fa5c532641730d3
Nico-T-Ihle <n.ihle2003@gmail.com>
2025-01-18 22:58:50 +0100

    upload sorting algo

---
+++ Contents.swift
@@ -0,0 +1,46 @@
+/*
+ 
+Selection explanation: -------------------------------------------------------------------------------
+ 
+ The Selection Sort algorithm sorts an array by repeatedly selecting the smallest element from the
+ unsorted part of the array and moving it to the current position. This is done in several passes
+ until the entire array is sorted.
+ 
+Task: ------------------------------------------------------------------------------------------------
+ 
+ Write a function called selectionSort that takes a list (array) of integers as input and sorts the
+ array in ascending order. Use the following algorithm:
+ 
+    1. iterate through the array and find the smallest element in the unsorted area of the array in each step.
+    2. swap the smallest element with the first element of the unsorted area.
+    3. repeat this process until the entire array is sorted.
+ 
+ */
+
+
+func selectionSort(list: Array<Int>) -> Array<Int>? {
+    
+    var sortedList = list
+    
+    for indexValue in 0..<sortedList.count {
+        
+        var minIndex = indexValue
+       
+        for i in (indexValue + 1)..<sortedList.count {
+            if sortedList[i] < sortedList[minIndex] {
+                minIndex = i
+            }
+        }
+        if minIndex != indexValue {
+            sortedList.swapAt(indexValue, minIndex)
+        }
+       
+    }
+
+    return sortedList
+}
+
+
+var listThatShouldBeSorted = [16,24,51,3,22,11]
+var sortedList = selectionSort(list: listThatShouldBeSorted)
+print(sortedList)
